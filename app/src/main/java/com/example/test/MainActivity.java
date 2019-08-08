41b8e64bef89977b9f13c68e2c524f144c3cfce8
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2019-08-08 18:03:16 -0500

    Connecting ListView to ArrayList
    Xml listview design render  array data.
    Connecting the bridge

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //connecting listview with Arraylist
 
         ListView listView =  findViewById(R.id.list_view);
 
