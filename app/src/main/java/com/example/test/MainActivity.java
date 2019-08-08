dde83335deaab19258d256ddfc09cfb0dcbc55f6
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2019-08-08 18:28:05 -0500

    On Click event in ListView

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.test;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -26,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,people);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                adapterView.setVisibility(View.GONE);
+            }
+        });
 
     }
 }
